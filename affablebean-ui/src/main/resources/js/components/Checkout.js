369713e5a7cb86673ef237b234de1764f5add428
Tony Wu <tony.wu90@yahoo.com>
2019-01-11 21:28:11 -0600

    checkout layout: initial fields mockup

--- affablebean-ui/src/main/resources/js/components/Checkout.js
+++ affablebean-ui/src/main/resources/js/components/Checkout.js
@@ -3,9 +3,31 @@ import React from 'react';
 const Checkout = () => {
     return (
         <div className="container">
-            <div className="row">
-                <h1>Page under development</h1>
-            </div>
+            <form>
+                <div className="form-group row">
+                    <label className="col-md-3 col-form-label">Total</label>
+                    <div className="col-md-9">
+                        <span className="text-primary">0 XLM</span>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-md-3 col-form-label">Email</label>
+                    <div className="col-md-9">
+                        <input type="text" className="form-control"/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="col-md-3 col-form-label">Source Account</label>
+                    <div className="col-md-9">
+                        <input type="text" className="form-control"/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <div className="offset-3 col-md-9 text-right">
+                        <input type="button" value="Confirm Payment" className="btn btn-warning"/>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 };
